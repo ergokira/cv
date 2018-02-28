8a625244e97bd15de0cd3258bc0f46e7b9b23005
josuegb15 <josueg@thehangar.cr>
2018-02-28 16:12:37 -0600

    version 2

--- app/js/bin/init.js
+++ app/js/bin/init.js
@@ -2,7 +2,6 @@
   $(function(){
 
   	var options = [
-    	{selector: '.abilities', offset: 300, callback: function(el) { Materialize.showStaggeredList($(el)); } },
     	{selector: 'footer', offset: 300, callback: function(el) { Materialize.showStaggeredList($(el)); } }
   	];
   	Materialize.scrollFire(options);
